1f301d3909074d936a1846e51ca0d42ff3e99884
nattawat <frank.nattawat@hotmail.com>
2023-11-30 15:28:47 +0700

    feat:complete ex conditional logic

--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,11 @@
 //Exercise 3
 // Ternary Operator
 let lightBulbStatus = "Off";
+lightBulbStatus === "On"
+? (lightBulbStatus = "On") 
+: lightBulbStatus === "Off"
+? (lightBulbStatus = "Off")
+: (lightBulbStatus = "Please choose the correct input (On/Off)");
 
+console.log(lightBulbStatus);
 //Start coding here
--- ex-4.js
+++ ex-4.js
@@ -2,4 +2,14 @@
 // Switch Statement
 let lightBulbStatus = "On";
 
+switch (lightBulbStatus) {
+    case "On":
+    lightBulbStatus = "On";
+    break;
+    case "Off":
+    lightBulbStatus = "Off";
+    break;
+
+}
+console.log(lightBulbStatus)
 //Start coding here
